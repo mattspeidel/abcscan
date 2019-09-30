db0b1aaae507a5d77c2cccab581e307aa8d1f3e2
mattspeidel <mattspeidel@gmail.com>
2019-09-30 13:19:03 -0400

    test logic for accessing abc api

--- scan.py
+++ scan.py
@@ -1,1 +1,7 @@
 import requests
+
+response = requests.get('http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu=27001')
+
+json_response = response.json()
+
+store_quantities = json_response['store_quantities']
