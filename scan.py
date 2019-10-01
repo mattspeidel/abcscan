ef595a3760d8a402477cb99ed84c8045bf1ae5a7
mattspeidel <mattspeidel@gmail.com>
2019-10-01 16:04:40 -0400

    hard coded sku list can be used to check quantities; will print name of item and then if there's any quantity will print the address and amount in stock

--- scan.py
+++ scan.py
@@ -1,7 +1,19 @@
 import requests
 
-response = requests.get('http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu=27001')
+# response = requests.get('http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu=27001')
 
-json_response = response.json()
+# json_response = response.json()
 
-store_quantities = json_response['store_quantities']
+# store_quantities = json_response['store_quantities']
+
+sku_list = [27169, 20590, 20595, 20594, 27090, 27118, 25568, 20592, 20570, 20752, 20715, '00013', 27001, 20020]
+
+for sku in sku_list:
+    response = requests.get(f'http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu={sku}')
+    print(response.json()['plu']['description'])
+    if response.json()['store_quantities'] == []:
+        print('NO STOCK')
+    else:
+        for store in response.json()['store_quantities']:
+            print(store['address'])
+            print(store['qoh'])
