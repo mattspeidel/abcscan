1c042b41709c3dbf81b66a13dd4f9bf1541d7c20
mattspeidel <mattspeidel@gmail.com>
2019-10-03 13:49:40 -0400

    scanner now reads from file containing skus

--- scan.py
+++ scan.py
@@ -1,12 +1,7 @@
 import requests
 
-# response = requests.get('http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu=27001')
-
-# json_response = response.json()
-
-# store_quantities = json_response['store_quantities']
-
-sku_list = [27169, 20590, 20595, 20594, 27090, 27118, 25568, 20592, 20570, 20752, 20715, '00013', 27001, 20020]
+with open('skulist.txt') as file:
+    sku_list = file.read().splitlines()
 
 for sku in sku_list:
     response = requests.get(f'http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu={sku}')
