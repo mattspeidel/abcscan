da58a61cbc1c2547bb2486989c9b093ce390ad60
mattspeidel <mattspeidel@gmail.com>
2019-10-10 13:41:10 -0400

    add scheduling

--- scan.py
+++ scan.py
@@ -1,43 +1,55 @@
 import requests
 import os
 from twilio.rest import Client
+import schedule
+import time
 
 account_sid = os.getenv('TWILSID')
 auth_token = os.getenv('TWILTOKEN')
 client = Client(account_sid, auth_token)
-newstock = []
-output = ''
 
-with open('skulist.txt') as file:
-    sku_list = file.read().splitlines()
 
-for sku in sku_list:
-    response = requests.get(f'http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu={sku}')
-    print(response.json()['plu']['description'])
-    if response.json()['store_quantities'] == []:
-        print('NO STOCK')
+def scanner():
+    newstock = []
+    output = ''
+    with open('skulist.txt') as file:
+        sku_list = file.read().splitlines()
+
+    for sku in sku_list:
+        response = requests.get(f'http://durhamabc.tagretail.com/transack/live_qoh/locations.json?plu={sku}')
+        print(response.json()['plu']['description'])
+        if response.json()['store_quantities'] == []:
+            print('NO STOCK')
+        else:
+            for store in response.json()['store_quantities']:
+                print(store['address'])
+                print(store['qoh'])
+            newstock.append(response.json()['plu']['description'])
+            output += response.json()['plu']['description'] + " "
+
+    with open('instock.txt') as oldstock:
+        comparestock = oldstock.read().splitlines()
+
+    if sorted(newstock) == sorted(comparestock):
+        print('---NO CHANGES---')
     else:
-        for store in response.json()['store_quantities']:
-            print(store['address'])
-            print(store['qoh'])
-        newstock.append(response.json()['plu']['description'])
-        output += response.json()['plu']['description'] + " "
+        with open('instock.txt', 'w') as instock:
+            for item in newstock:
+                instock.write('%s\n' % item)
+        print('---CHANGES WERE MADE---')
+        message = client.messages \
+                    .create(
+                        body=output + 'are in stock now',
+                        from_=os.getenv('MYTWILNUMBER'),
+                        to=os.getenv('MYPHONENUMBER')
+                    )
 
-with open('instock.txt') as oldstock:
-    comparestock = oldstock.read().splitlines()
+        print(message.sid)
 
-if sorted(newstock) == sorted(comparestock):
-    print('---NO CHANGES---')
-else:
-    with open('instock.txt', 'w') as instock:
-        for item in newstock:
-            instock.write('%s\n' % item)
-    print('---CHANGES WERE MADE---')
-    message = client.messages \
-                .create(
-                     body=output + 'are in stock now',
-                     from_=os.getenv('MYTWILNUMBER'),
-                     to=os.getenv('MYPHONENUMBER')
-                 )
+schedule.every().day.at("08:00").do(scanner)
+schedule.every().day.at("12:00").do(scanner)
+schedule.every().day.at("16:00").do(scanner)
 
-    print(message.sid)
+while True:
+    schedule.run_pending()
+    time.sleep(3600)
